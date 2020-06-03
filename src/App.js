ccde60f5f4bc607ce26899bd1045b34dc9a3d0d2
Avi Jain <avijain1001@gmail.com>
2020-06-04 00:01:15 +0530

    Changes Made : App.js , TodoItem.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
 function App() {
   return (
-    <div>
-      <input type="checkbox" />
-      <p>Placeholder Text</p>
-
-      <input type="checkbox" />
-      <p>Placeholder Text</p>
-
-      <input type="checkbox" />
-      <p>Placeholder Text</p>
-
-      <input type="checkbox" />
-      <p>Placeholder Text</p>
+    <div className="todo-list">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-function App() {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeofday;
-
-  if (hours < 12) {
-    timeofday = "morning";
-  } else if (hours >= 12 && hours < 17) {
-    timeofday = "afternoon";
-  } else {
-    timeofday = "night";
-  }
-
-  return <h1>Good {timeofday}</h1>;
-}
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
