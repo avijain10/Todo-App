f640669efd7f8a63a427be1bb87780c11ad190da
Avi Jain <avijain1001@gmail.com>
2020-06-04 19:32:59 +0530

    Modified: App.js & Added: jokesData.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React from "react";
 import Joke from "./Joke";
+import jokesData from "./jokesData";
 
 function App() {
-  return (
-    <div>
-      <Joke
-        question="What is the first lang used for frontend?"
-        answer="HTML"
-      />
-      <Joke
-        question="What is the second lang used for frontend?"
-        answer="CSS"
-      />
-      <Joke
-        question="What is the third lang used for frontend?"
-        answer="Javascript"
-      />
-      <Joke
-        question="What is the fourth lang used for frontend?"
-        answer="ES6"
-      />
-      <Joke
-        question="What is the fifth lang used for frontend?"
-        answer="React"
-      />
-    </div>
-  );
+  const jokeComponents = jokesData.map((joke) => (
+    <Joke key={Joke.id} question={joke.question} answer={joke.answer} />
+  ));
+
+  return <div>{jokeComponents}</div>;
 }
 export default App;
---
+++ src/jokesData.js
@@ -0,0 +1,29 @@
+const jokesData = [
+  {
+    id: 1,
+    question: "What is the first lang used for frontend?",
+    answer: "HTML",
+  },
+  {
+    id: 2,
+    question: "What is the second lang used for frontend?",
+    answer: "CSS",
+  },
+  {
+    id: 3,
+    question: "What is the third lang used for frontend?",
+    answer: "Javascript",
+  },
+  {
+    id: 4,
+    question: "What is the fourth lang used for frontend?",
+    answer: "ES6",
+  },
+  {
+    id: 5,
+    question: "What is the fifth lang used for frontend?",
+    answer: "React",
+  },
+];
+
+export default jokesData;
