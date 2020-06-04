a4944360d686e5f9be866727aea45f64110f3cf9
Avi Jain <avijain1001@gmail.com>
2020-06-04 22:54:07 +0530

    Modified : index.js

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,7 @@ import React from "react";
 function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input type="checkbox" />
+      <input type="checkbox" checked={props.todoitem.completed} />
       <p>{props.todoitem.text}</p>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header username="Avi" />
+        <Greeting />
+      </div>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <p>Welcome, {this.props.username}</p>
+      </header>
+    );
+  }
+}
+
+class Greeting extends Component {
+  render() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeofday;
+
+    if (hours < 12) {
+      timeofday = "morning";
+    } else if (hours >= 12 && hours < 17) {
+      timeofday = "afternoon";
+    } else {
+      timeofday = "night";
+    }
+
+    return <h1>Good {timeofday}</h1>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
